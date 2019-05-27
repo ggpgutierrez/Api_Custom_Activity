06e6aa4bcfc2915b22c395a0aa8aa1e9cb8902b2
ggpgutierrez <ggp.134@hotmail.com>
2019-05-27 10:25:50 -0300

    Passagem req,body

--- server/controllers/controllerJourney.js
+++ server/controllers/controllerJourney.js
@@ -2,7 +2,7 @@ const journeyService = require('../services/servicesJourney')
 
 const postJourney = (req, res) => {
     console.log(req.body);
-    const email = req.body[inArguments][Email];
+    const email = req.body;
    
     try{
         journeyService.createCustomer(email);
